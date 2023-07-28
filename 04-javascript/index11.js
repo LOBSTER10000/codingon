e3b46b89a59df3a3eeec47ecacdb7ab048f825f7
LOBSTER10000 <ghh112034@gmail.com>
2023-07-28 22:17:31 +0900

    commit

--- 04-javascript/index11.js
+++ 04-javascript/index11.js
@@ -47,3 +47,62 @@ btn4.addEventListener('click', function (e) {
   this.style.background = 'gold'; // btn4로 접근
   this.style.color = '#000';
 });
+
+const btn = document.querySelector('button');
+const input = document.querySelector('input');
+
+btn.addEventListener('click', function (e) {
+  // 이벤트 객체
+  console.log(e); //이벤트 객체에 대한 정보 출력
+});
+
+input.addEventListener('keydown', function (e) {
+  console.log(e.code); // 눌러진 키의 고유 코드
+  console.log(e.key); // input에 입력된 값
+  if (e.code == 'ArrowUp') {
+    console.log('업');
+  } else if (e.code == 'ArrowDown') {
+    console.log('다운');
+  }
+});
+
+//폼 관련 이벤트
+const todoForm = document.querySelector('#todo-form');
+const todos = document.querySelector('.todos');
+
+todoForm.addEventListener('submit', function (e) {
+  console.log('submit');
+  e.preventDefault(); // 폼 submit 이벤트가 새로 고침 되는 것을 막음
+  // 폼 제출을 막음
+
+  const todoInput = document.querySelector('input[name="todo"]');
+  //   console.log(todoInput);
+  //   console.dir(todoInput.value); // 인풋에 입력된 값을 의미함
+  //   console.log(todoInput.value); // input에 입력된 값
+
+  const newTodo = todoInput.value.trim();
+
+  if (newTodo !== '') {
+    const newTodoLi = document.createElement('li');
+    newTodoLi.append(newTodo); //<li>input 입력값</li>
+    todos.append(newTodoLi);
+  }
+
+  //input 창 초기화
+
+  todoInput.value = '';
+});
+
+//change는 input요소에서 변경이 일어나고 다른 요소를 클릭했을때 일어나는 이벤트
+const chgInput = document.querySelector('#change-input');
+chgInput.addEventListener('change', function (e) {
+  console.log('change!!!', e);
+});
+
+// input에 변화가 감지될 때마다 이벤트가 일어남
+// 실시간 텍스트 동기화도 마찬가지로 사용가능
+chgInput.addEventListener('input', function (e) {
+  console.log('changing!!', e);
+  const div = document.querySelector('.intro');
+  div.textContent = this.value;
+});
