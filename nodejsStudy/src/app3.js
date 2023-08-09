1ce034459df867c090ed28b8afbdb4fa5827e6d1
LOBSTER10000 <ghh112034@gmail.com>
2023-08-09 13:54:02 +0900

    commit

--- nodejsStudy/src/app3.js
+++ nodejsStudy/src/app3.js
@@ -5,7 +5,7 @@ app.set('views', './views');
 app.set('view engine', 'ejs');
 
 //미들웨어 등록
-let home = require('./routes/home');
+let home = require('./src/routes/home');
 app.use('/', home);
 
 module.exports = app;
--- nodejsStudy/src/bin/www.js
+++ nodejsStudy/src/bin/www.js
@@ -1,4 +1,4 @@
-let app = require('../app3');
+let app = require('../../app3');
 
 app.listen(3000, () => {
   console.log('서버연결에 성공했습니다');
