df1eac9cbda4a7b45598d0bb9fc6c4b5e209741a
LOBSTER10000 <ghh112034@gmail.com>
2023-08-09 13:07:53 +0900

    coo

---
+++ nodepractice/node2.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a + b;
+}
+
+const PI = 3.14;
+const E = 2.78;
+
+module.exports = {
+  add,
+  PI,
+  E,
+};
---
+++ nodepractice/practice.js
@@ -0,0 +1,3 @@
+import { PI, E } from './node3.js';
+import add from './node4.js';
+console.log(add(PI, E));
