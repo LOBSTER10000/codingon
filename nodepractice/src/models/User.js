e3dd78b117a4ac83d41dabc73931563500069cb5
LOBSTER10000 <ghh112034@gmail.com>
2023-08-14 12:38:26 +0900

    commit

--- nodepractice/src/models/User.js
+++ nodepractice/src/models/User.js
@@ -1,21 +1,30 @@
 class User {
-  #users = {
+  static users = {
     id: ['lobster', 'lob', 'abc'],
     pass: ['1', '2', '3'],
     name: ['랍스터', '랍', '에이'],
   };
 
-  getUsersId() {
-    return this.#users.id;
-  }
-
-  getUsersPass() {
-    return this.#users.pass;
-  }
-
-  getUsersName() {
-    return this.#users.name;
+  static getUsers(...fields) {
+    const users = this.users;
+    const newUsers = fields.reduce((newUsers, field) => {
+      if (users.hasOwnProperty(field)) {
+        newUsers[field] = users[field];
+      }
+      return newUsers;
+    }, {});
+    return newUsers;
   }
 }
+
+// class User {
+//   constructor(_userId, _userPass, _userName) {
+//     if (_userId && _userPass && _userName) {
+//       this._userId = userId;
+//       this._userPass = userPass;
+//       this._userName = userName;
+//     }
+//   }
+// }
 
 module.exports = User;
--- nodepractice/src/routes/home/home.ctrl.js
+++ nodepractice/src/routes/home/home.ctrl.js
@@ -8,27 +8,27 @@ const output = {
   },
 };
 
-const User = require('../../models/User.js');
-const us = new User();
+const users = require('../../models/User.js');
+users.getUsers();
 
 const input = {
   login: function (req, res) {
     const id = req.body.id;
     const pass = req.body.pass;
 
-    console.log(us.getUsersId());
+    const user = users.getUsers('id', 'pass');
     const response = {};
-    if (us.getUsersId().includes(id)) {
-      const idx = us.getUsersId().indexOf(id);
-      if (us.getUsersPass()[idx] === pass) {
+    if (user.id.includes(id)) {
+      const idx = user.id.indexOf(id);
+      if (user.pass[idx] === pass) {
         response.success = true;
-        response.msg = '로그인에 성공했습니다';
-        return res.json(response) + console.log(us.getUsersName()[idx]);
+        response.msg = '로그인에 성공하셨습니다';
+        return res.json(response);
       }
     }
     response.success = false;
     response.msg = '로그인에 실패했습니다';
-    return res.json(response) + console.log(us.getUsersName());
+    return res.json(response);
   },
 };
-module.exports = { output, User, input };
+module.exports = { output, users, input };
