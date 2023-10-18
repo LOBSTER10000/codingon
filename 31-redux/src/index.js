62a6c679f37a17a23e80dee9a883c497e7134d84
LOBSTER10000 <ghh112034@gmail.com>
2023-10-18 14:25:25 +0900

    study: redux 적용

--- 31-redux/src/index.js
+++ 31-redux/src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
+import App2 from './App2';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// state 초기값 정의
+const initialState = {
+  number: 50,
+};
+
+// reducer : 액션의 타입에 따라 변화하는 함수
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'PLUS':
+      return { number: state.number + 1 };
+    case 'MINUS':
+      return { number: state.number - 1 };
+    default:
+      return state;
+  }
+};
+
+// store 정의 : 전역 상태를 관리하는 공간 (하나의 프로젝트에 하나만!)
+const store = configureStore({ reducer: reducer }, composeWithDevTools());
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App2 />
+    </Provider>
   </React.StrictMode>
 );
